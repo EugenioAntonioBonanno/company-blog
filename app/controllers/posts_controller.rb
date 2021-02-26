72300b096203e6c72b41355119cfcb6d1340af9c
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-26 18:42:05 +0800

    add httparty client, accept response, itterate over it using slim

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+  include HTTParty
+
   def index
+    response = HTTParty.get('http://localhost:4000/posts')
+    @posts = response.parsed_response['posts']
   end
 
   def new
