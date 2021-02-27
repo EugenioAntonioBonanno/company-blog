8cf013e0c3b5b124037b3ea658fac5b021614379
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-27 14:09:43 +0800

    add working create action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  require 'json'
 
   def index
     @posts = Post.all
@@ -9,8 +10,24 @@ class PostsController < ApplicationController
   end
 
   def create
+    # sends a POST request to http://localhost:4000/posts
+    # In the following format: { "post": {"title": params[post][title],"body": params[post][body] }}
+
+    if Post.create(post: { post: { title: post_params[:title], body: post_params[:body] } })
+      flash[:success] = 'Post created'
+    else
+      flash[:danger] = 'Unable to create post'
+    end
+
+    redirect_to root_url
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
