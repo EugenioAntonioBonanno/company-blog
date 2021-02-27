211ce1eedb3b23ed78abed38d006e9b8e1742512
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-27 16:14:00 +0800

    clean up post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,5 @@
 class Post < ActiveResource::Base
   self.site = 'http://localhost:4000'
   self.include_format_in_path = false
-
-  protected
-
-  def validate
-    errors.add("title", "title is required") unless title.length > 5
-  end
+  
 end
