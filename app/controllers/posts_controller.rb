9335981fc61dfd3f0d2d2a177cd4800d222fde10
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-27 14:15:55 +0800

    clean up posts controller, add client-side validation to post form, disable submit button on click

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  require 'json'
 
   def index
     @posts = Post.all
@@ -10,8 +9,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    # sends a POST request to http://localhost:4000/posts
-    # In the following format: { "post": {"title": params[post][title],"body": params[post][body] }}
+    # Sends a POST request to http://localhost:4000/posts
+    # Uses the following format: { "post": {"title": params[post][title], "body": params[post][body] } }
 
     if Post.create(post: { post: { title: post_params[:title], body: post_params[:body] } })
       flash[:success] = 'Post created'
@@ -20,9 +19,6 @@ class PostsController < ApplicationController
     end
 
     redirect_to root_url
-  end
-
-  def show
   end
 
   private
