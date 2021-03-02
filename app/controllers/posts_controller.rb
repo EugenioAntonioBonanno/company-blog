0f3d886c1de6d12c3e57e90fe0f85a81a87cbd04
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-03-02 13:38:29 +0800

    add class method for returning posts in correct order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.sort_by(&:created_at).reverse
+    @posts = Post.ordered_posts
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveResource::Base
   # VALIDATIONS
   # Server has validations requiring title and body to be present.
   # Client side has validations requiring title to have a min length of 3, and body min length of 15
+
+  class << self
+    def ordered_posts
+      all.sort_by(&:created_at).reverse
+    end
+  end
 end
