b445533deb966fb2e6f42dc15483057c6f106643
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-03-02 13:52:55 +0800

    add system test for order and clean up system and r-spec tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  valid_post_title = 'Valid Title'
-  valid_post_body = 'This is a write up for the post that should always be valid.'
 
+  before(:context) do
+    @valid_post_title = 'Valid Title'
+    @valid_post_body = 'This is a write up for the post that should always be valid.'
+  end
+  
   # Example of necessary format "Post.create(post: { post: { title: post_params[:title], body: post_params[:body] } })"
   context 'validation tests' do
     original_post_count = Post.all.length
     it 'ensures title presence' do
-      Post.new(post: { post: { title: valid_post_title } }).save
+      Post.new(post: { post: { title: @valid_post_title } }).save
       expect(original_post_count == Post.all.length).to eq(true)
     end
 
     it 'ensures body presence' do
-      Post.new(post: { post: { body: valid_post_body } }).save
+      Post.new(post: { post: { body: @valid_post_body } }).save
       expect(original_post_count == Post.all.length).to eq(true)
     end
 
     it 'should save successfully' do
-      Post.new(post: { post: { title: valid_post_title, body: valid_post_body } }).save
+      Post.new(post: { post: { title: @valid_post_title, body: @valid_post_body } }).save
       expect(original_post_count < Post.all.length).to eq(true)
     end
   end
--- spec/system/creating_blog_posts_spec.rb
+++ spec/system/creating_blog_posts_spec.rb
@@ -5,17 +5,24 @@ RSpec.describe "CreatingBlogPosts", type: :system do
     driven_by(:rack_test)
   end
 
+  before(:context) do
+    @title = 'A blog title'
+    @body = 'This is a bunch of stuff to create a body for a post'
+  end
+
   it 'saves and displays created blog post' do
     visit '/posts/new'
 
-    fill_in 'post[title]', with: 'A blog title'
-    fill_in 'post[body]', with: 'This is a bunch of stuff to create a body for a post'
+    fill_in 'post[title]', with: @title
+    fill_in 'post[body]', with: @body
 
     find('input[name="commit"]').click
 
-    expect(page).to have_content('A blog title')
-    expect(page).to have_content('This is a bunch of stuff to create a body')
+    expect(page).to have_content(@title)
+    expect(page).to have_content(@body)
 
-    post = Post.all
+    post = Post.ordered_posts.first
+    expect(post.title).to eq(@title)
+    expect(post.body).to eq(@body)
   end
 end
