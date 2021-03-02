38f1b8491c2cf39c0af4a1f0a4726c7b258a0660
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-03-02 13:35:28 +0800

    add first system test with proper rspec selectors

--- spec/system/creating_blog_posts_spec.rb
+++ spec/system/creating_blog_posts_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe "CreatingBlogPosts", type: :system do
   it 'saves and displays created blog post' do
     visit '/posts/new'
 
-    fill_in 'Title', with: 'A blog title'
-    fill_in 'Body', with: 'This is a bunch of stuff to create a body'
+    fill_in 'post[title]', with: 'A blog title'
+    fill_in 'post[body]', with: 'This is a bunch of stuff to create a body for a post'
 
-    click_on 'Create Post'
+    find('input[name="commit"]').click
 
     expect(page).to have_content('A blog title')
     expect(page).to have_content('This is a bunch of stuff to create a body')
+
+    post = Post.all
   end
 end
