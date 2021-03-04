fda2d7bde6e8f1eca6e0b7bfe2d8da0cf5767593
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-03-04 15:26:54 +0800

    clear form after submitting, fix name clash between post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,20 @@ class CommentsController < ApplicationController
 
 
   def create
-    comment = Comment.new(comment: { comment: { name: comment_params[:name], body: comment_params[:body] } })
+    comment = Comment.new(comment: { comment: { name: comment_params[:name], body: comment_params[:comment_body] } })
     comment.prefix_options[:post_id] = comment_params[:post_id]
 
     if comment.save
-
+      respond_to do |format|
+        format.js {}
+      end
     end
   end
 
   private
 
   def comment_params
-    params.require(:post).permit(:name, :body, :post_id)
+    params.require(:post).permit(:name, :comment_body, :post_id)
   end
 end
 
